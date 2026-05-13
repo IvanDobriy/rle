39553f5b4a701db7370115d1fad0815157e8b2dc
idanilchenko@altsoft.biz <idanilchenko@altsoft.biz>
2026-05-13 13:35:15 +0300

    fill greeting

--- domain/src/main/java/ru/otus/danilchenko/domain/use_case/GreetingUseCase.java
+++ domain/src/main/java/ru/otus/danilchenko/domain/use_case/GreetingUseCase.java
@@ -4,6 +4,8 @@ import ru.otus.danilchenko.domain.command.Interaction;
 
 public class GreetingUseCase {
     public void execute(Interaction interaction) {
-        interaction.write("hello");
+        interaction.write("Welcome to RLE Compressor!");
+        interaction.write("A CLI tool for compressing and decompressing files using the Run-Length Encoding algorithm.");
+        interaction.write("Use --help to see available commands.");
     }
 }
